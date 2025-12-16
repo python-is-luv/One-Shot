d51103036968480697adf9f822de30de4dd7aec9
Bhardwaj <bhardwaj.14.mit@gmail.com>
2025-12-16 08:17:37 +0530

    Added gun, its cs collisions and script where it can throw bullet etc...

---
+++ scripts/gun.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+func shoot():
+	const BULLET = preload("res://scenes/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new _bullet.global_rotation = %Shooting_Point.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+	
+func _on_timer_timeout() -> void:
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target = enemies_in_range.size() > 0
+		look_at(target.global_position)
+		shoot()
