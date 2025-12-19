bd4d36d1d6e63cda4d41f3803435a99afaaa04ee
Bhardwaj <bhardwaj.14.mit@gmail.com>
2025-12-19 17:38:21 +0530

    Added Music to the game(main menu and the main game) and also zoomed the camera for a better view...

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,10 +6,5 @@ func ready():
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
